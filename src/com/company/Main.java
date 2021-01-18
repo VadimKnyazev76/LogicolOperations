6157eb59359a25a8404fe4d0f9d7697513556826
VadimKnyazev76 <vadim.knyazev@gmail.com>
2021-01-18 10:25:18 +0200

    Changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,23 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-    int coutage = 30;
+    int coutage = 15;
     int salary = 100;
     double premium = 15;
     if (coutage < 5)  {premium = salary*0.1;}
     if (coutage > 5 && coutage < 10)  {premium = salary*0.15;}
-        if (coutage > 10 && coutage < 15) {
+        if (coutage >= 10 && coutage < 15) {
             premium = salary * 0.25;
         }
-            if (coutage > 15 && coutage < 20) {
+            if (coutage >= 15 && coutage < 20) {
                 premium = salary * 0.35;
             }
-                if (coutage > 20 && coutage < 25) {
+                if (coutage >= 20 && coutage < 25) {
                     premium = salary * 0.45;
                 }
-                    if (coutage > 25) { premium = salary*0.5;}
+                    if (coutage >= 25) { premium = salary*0.5;}
 
-                    System.out.println(salary);
+                    System.out.println(premium);
 
 
 
